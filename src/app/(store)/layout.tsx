b7ee5ec675656ce2bbd25c25eecdbf9beb79ef94
sadik03 <mssadik03@gmail.com>
2025-05-08 18:42:48 +0530

    bug fix

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -2,7 +2,6 @@
 import { Provider } from "react-redux";
 
 import StoreNavBar from "@/domains/store/shared/components/navbar";
-import Warning from "@/domains/store/shared/components/warning";
 import { shoppingCartStore } from "@/store/shoppingCart";
 
 import StoreFooter from "../../domains/store/shared/components/footer/index";
